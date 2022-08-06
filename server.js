a4d48e91991741162effb18ab16963d217949c5d
PRINCE <94777825+Prince-Codemon@users.noreply.github.com>
2022-08-06 18:01:21 +0530

    code added

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const bodyparser = require('body-parser')
 const app = express()
 const cors = require('cors')
 const cookieparser =require('cookie-parser')
+const path = require('path')
 require('dotenv').config()
 const PORT = process.env.PORT || 5000
 require('../backend/Config/db')
 app.use(cookieparser())
 app.use(cors({
-    origin:process.env.origin,
+    origin:process.env.origin || "http://localhost:3000",
     credentials:true,
     allowedHeaders:['Content-Type','Authorization','Accept','Origin','X-Requested-With','Access-Control-Allow-Origin','Access-Control-Allow-Credentials', 'cookie'],
     methods:['GET','POST','PUT','DELETE','OPTIONS'],
@@ -17,12 +18,20 @@ const router = require('../backend/routes')
 app.use(bodyparser.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname , '/frontend/build')));
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"frontend", "build","index.html"))
+  })
+}
+else{
+    app.get("/",(req,res)=>{
+        res.send('hello world')
+    })
+}
 
 app.use(router)
 
-if(process.env.NODE_ENV ==="production"){
-    app.use(express.static("frontend/build"))
-}
 
 
 app.listen(PORT,()=>console.log('Listening ON PORT ', PORT))
